884f8c933810dd06930e9c17709c95b488d020bf
julia-anne <julia.robinson@spareroom.co.uk>
2025-04-25 21:39:21 +0100

    About page and homepage link

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+// import Image from "next/image";
+
+export default function About() {
+  return (
+    <div>
+      <main>
+        <p>This is the about page</p>
+      </main>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 // import Image from "next/image";
+import Link from "next/link";
 import Input from "../components/input";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Link href="/about">About</Link>
       <main className={styles.main}>
         <Input></Input>
       </main>
